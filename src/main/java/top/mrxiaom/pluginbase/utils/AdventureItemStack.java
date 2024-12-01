71597880b9fc87bae7546a3ca91ea4dd9f69030d
人間工作 <coolxiaom95@gmail.com>
2024-12-01 20:03:02 +0800

    set method supports mc less than 1.14

--- src/main/java/top/mrxiaom/pluginbase/utils/AdventureItemStack.java
+++ src/main/java/top/mrxiaom/pluginbase/utils/AdventureItemStack.java
@@ -9,6 +9,7 @@ import de.tr7zw.changeme.nbtapi.iface.ReadableNBT;
 import de.tr7zw.changeme.nbtapi.utils.MinecraftVersion;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -42,8 +43,13 @@ public class AdventureItemStack {
     public static void setItemDisplayName(ItemStack item, String name) {
         if (item == null) return;
         Component displayName = miniMessage(name);
-        String json = GsonComponentSerializer.gson().serialize(displayName);
-        setItemDisplayNameByJson(item, json);
+        if (MinecraftVersion.isAtLeastVersion(MinecraftVersion.MC1_13_R1)) {
+            String json = GsonComponentSerializer.gson().serialize(displayName);
+            setItemDisplayNameByJson(item, json);
+        } else {
+            String legacy = LegacyComponentSerializer.legacySection().serialize(displayName);
+            setItemDisplayNameByJson(item, legacy);
+        }
     }
 
     @Nullable
@@ -99,9 +105,16 @@ public class AdventureItemStack {
     public static void setItemLore(ItemStack item, List<String> lore) {
         if (item == null) return;
         List<String> json = new ArrayList<>();
-        for (String s : lore) {
-            Component line = miniMessage(s);
-            json.add(GsonComponentSerializer.gson().serialize(line));
+        if (MinecraftVersion.isAtLeastVersion(MinecraftVersion.MC1_14_R1)) {
+            for (String s : lore) {
+                Component line = miniMessage(s);
+                json.add(GsonComponentSerializer.gson().serialize(line));
+            }
+        } else {
+            for (String s : lore) {
+                Component line = miniMessage(s);
+                json.add(LegacyComponentSerializer.legacySection().serialize(line));
+            }
         }
         setItemLoreByJson(item, json);
     }
