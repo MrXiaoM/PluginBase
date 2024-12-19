dc06c95c97bc434566c6e2c159f3dd2dcfb3caf3
人間工作 <coolxiaom95@gmail.com>
2024-12-19 12:29:34 +0800

    use local file because jitpack is down and mirror wrong response

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,8 @@ dependencies {
     compileOnly("org.spigotmc:spigot-api:1.20-R0.1-SNAPSHOT")
     
     compileOnly("net.milkbowl.vault:VaultAPI:1.7")
-    compileOnly("com.github.LoneDev6:API-ItemsAdder:3.6.3-beta-14")
+    // compileOnly("com.github.LoneDev6:API-ItemsAdder:3.6.3-beta-14")
+    compileOnly(files("libs/api-itemsadder-3.6.3-beta-14.jar"))
     compileOnly("me.clip:placeholderapi:2.11.6")
     compileOnly("com.zaxxer:HikariCP:4.0.3")
 
