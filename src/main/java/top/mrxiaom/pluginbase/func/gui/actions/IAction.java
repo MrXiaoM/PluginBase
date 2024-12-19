50579503d2718f2d459a1a567aac3192f6b139ae
人間工作 <coolxiaom95@gmail.com>
2024-12-20 01:29:08 +0800

    provide no replacements method

--- src/main/java/top/mrxiaom/pluginbase/func/gui/actions/IAction.java
+++ src/main/java/top/mrxiaom/pluginbase/func/gui/actions/IAction.java
@@ -4,5 +4,8 @@ import org.bukkit.entity.Player;
 import top.mrxiaom.pluginbase.utils.Pair;
 
 public interface IAction {
+    default void run(Player player) {
+        run(player, Pair.array(0));
+    }
     void run(Player player, Pair<String, Object>[] replacements);
 }
