1d3d2bfb564f04623ad7ad982d263fd1330684b6
人間工作 <coolxiaom95@gmail.com>
2025-04-14 17:12:22 +0800

    Update Pair.java

--- src/main/java/top/mrxiaom/pluginbase/utils/Pair.java
+++ src/main/java/top/mrxiaom/pluginbase/utils/Pair.java
@@ -2,6 +2,9 @@ package top.mrxiaom.pluginbase.utils;
 
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pair<K, V> {
     K key;
     V value;
@@ -15,7 +18,23 @@ public class Pair<K, V> {
         return key;
     }
 
+    public K key() {
+        return key;
+    }
+
+    public K left() {
+        return key;
+    }
+
     public void setKey(K key) {
+        this.key = key;
+    }
+
+    public void key(K key) {
+        this.key = key;
+    }
+
+    public void left(K key) {
         this.key = key;
     }
 
@@ -23,7 +42,23 @@ public class Pair<K, V> {
         return value;
     }
 
+    public V value() {
+        return value;
+    }
+
+    public V right() {
+        return value;
+    }
+
     public void setValue(V value) {
+        this.value = value;
+    }
+
+    public void value(V value) {
+        this.value = value;
+    }
+
+    public void right(V value) {
         this.value = value;
     }
 
@@ -34,6 +69,24 @@ public class Pair<K, V> {
 
     public static <K, V> Pair<K, V> of(K key, V value) {
         return new Pair<>(key, value);
+    }
+
+    public static List<String> replace(List<String> list, @Nullable Iterable<Pair<String, Object>> replacements) {
+        if (replacements == null) return new ArrayList<>(list);
+        List<String> result = new ArrayList<>();
+        for (String s : list) {
+            result.add(replace(s, replacements));
+        }
+        return result;
+    }
+
+    public static List<String> replace(List<String> list, Pair<String, Object> @Nullable [] replacements) {
+        if (replacements == null) return new ArrayList<>(list);
+        List<String> result = new ArrayList<>();
+        for (String s : list) {
+            result.add(replace(s, replacements));
+        }
+        return result;
     }
 
     public static String replace(String s, @Nullable Iterable<Pair<String, Object>> replacements) {
