16c2e69635d7bd9b12531c77c5ac729ef5ec10f4
人間工作 <coolxiaom95@gmail.com>
2025-05-26 15:22:57 +0800

    Create PlaceholdersExpansion.java

---
+++ src/main/java/top/mrxiaom/pluginbase/utils/PlaceholdersExpansion.java
@@ -0,0 +1,35 @@
+package top.mrxiaom.pluginbase.utils;
+
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+
+public abstract class PlaceholdersExpansion<T extends JavaPlugin> extends PlaceholderExpansion {
+    protected final T plugin;
+    protected PlaceholdersExpansion(T plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean register() {
+        try {
+            unregister();
+        } catch (Throwable ignored) {}
+        return super.register();
+    }
+
+    @Override
+    public @NotNull String getIdentifier() {
+        return plugin.getDescription().getName().toLowerCase();
+    }
+
+    @Override
+    public @NotNull String getAuthor() {
+        return String.join(", ", plugin.getDescription().getAuthors());
+    }
+
+    @Override
+    public @NotNull String getVersion() {
+        return plugin.getDescription().getVersion();
+    }
+}
