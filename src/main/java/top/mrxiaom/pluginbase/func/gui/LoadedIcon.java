28766305486cfc5dc605b996e16786a7acdbae0f
MrXiaoM <coolxiaom95@gmail.com>
2025-08-20 00:00:52 +0800

    add more functional methods into LoadedIcon

--- src/main/java/top/mrxiaom/pluginbase/func/gui/LoadedIcon.java
+++ src/main/java/top/mrxiaom/pluginbase/func/gui/LoadedIcon.java
@@ -1,6 +1,5 @@
 package top.mrxiaom.pluginbase.func.gui;
 
-import com.google.common.collect.Lists;
 import de.tr7zw.changeme.nbtapi.NBT;
 import org.bukkit.Material;
 import org.bukkit.configuration.ConfigurationSection;
@@ -18,46 +17,125 @@ import java.util.*;
 import static top.mrxiaom.pluginbase.actions.ActionProviders.loadActions;
 import static top.mrxiaom.pluginbase.func.gui.IModifier.fit;
 
+/**
+ * 通用界面图标配置
+ */
 public class LoadedIcon {
     private static final List<ITagProvider> tagProviders = new ArrayList<>();
-    private final boolean adventure;
+    private final boolean adventure = BukkitPlugin.getInstance().options.adventure();
+    /**
+     * 物品材质
+     */
     public final String material;
+    /**
+     * 物品数量
+     */
     public final int amount;
+    /**
+     * 物品显示名称
+     */
     public final String display;
+    /**
+     * 物品 Lore
+     */
     public final List<String> lore;
+    /**
+     * 物品是否发光，即物品是否添加一个附魔并隐藏，以产生附魔光泽
+     */
     public final boolean glow;
+    /**
+     * 物品的自定义模型标记
+     */
     public final Integer customModelData;
+    /**
+     * 物品的额外 NBT
+     */
     public final Map<String, String> nbtStrings;
+    /**
+     * 物品的额外 NBT
+     */
     public final Map<String, String> nbtInts;
+    /**
+     * 左键点击物品执行的操作
+     */
     public final List<IAction> leftClickCommands;
+    /**
+     * 右键点击物品执行的操作
+     */
     public final List<IAction> rightClickCommands;
+    /**
+     * Shift+左键点击物品执行的操作
+     */
     public final List<IAction> shiftLeftClickCommands;
+    /**
+     * Shift+右键点击物品执行的操作
+     */
     public final List<IAction> shiftRightClickCommands;
+    /**
+     * 鼠标悬停，Q键点击物品执行的操作
+     */
     public final List<IAction> dropCommands;
+    /**
+     * 物品的自定义标签，通过 ITagProvider 提供
+     */
     public final Object tag;
 
-    LoadedIcon(boolean adventure, String material, int amount, String display, List<String> lore, boolean glow, Integer customModelData, Map<String, String> nbtStrings, Map<String, String> nbtInts, List<IAction> leftClickCommands, List<IAction> rightClickCommands, List<IAction> shiftLeftClickCommands, List<IAction> shiftRightClickCommands, List<IAction> dropCommands, Object tag) {
-        this.adventure = adventure;
+    LoadedIcon(ConfigurationSection current) {
+        ConfigurationSection section;
+
+        String material, materialStr = current.getString("material");
+        if (materialStr != null) {
+            if (!materialStr.contains(":") && current.contains("data")) { // 兼容旧的选项
+                material = materialStr + ":" + current.getInt("data");
+            } else material = materialStr;
+        } else material = "PAPER";
         this.material = material.toUpperCase();
-        this.amount = amount;
-        this.display = display;
-        this.lore = lore;
-        this.glow = glow;
-        this.customModelData = customModelData;
-        this.nbtStrings = nbtStrings;
-        this.nbtInts = nbtInts;
-        this.leftClickCommands = leftClickCommands;
-        this.rightClickCommands = rightClickCommands;
-        this.shiftLeftClickCommands = shiftLeftClickCommands;
-        this.shiftRightClickCommands = shiftRightClickCommands;
-        this.dropCommands = dropCommands;
+
+        this.amount = current.getInt("amount", 1);
+        this.display = current.getString("display", "");
+        this.lore = current.getStringList("lore");
+        this.glow = current.getBoolean("glow");
+        this.customModelData = current.contains("custom-model-data") ? current.getInt("custom-model-data") : null;
+        this.nbtStrings = new HashMap<>();
+        section = current.getConfigurationSection("nbt-strings");
+        if (section != null) for (String key : section.getKeys(false)) {
+            nbtStrings.put(key, section.getString(key, ""));
+        }
+        this.nbtInts = new HashMap<>();
+        section = current.getConfigurationSection("nbt-ints");
+        if (section != null) for (String key : section.getKeys(false)) {
+            nbtInts.put(key, section.getString(key, ""));
+        }
+        this.leftClickCommands = loadActions(current, "left-click-commands", "left_click_commands");
+        this.rightClickCommands = loadActions(current, "right-click-commands", "right_click_commands");
+        this.shiftLeftClickCommands = loadActions(current, "shift-left-click-commands", "shift_left_click_commands");
+        this.shiftRightClickCommands = loadActions(current, "shift-left-click-commands", "shift_left_click_commands");
+        this.dropCommands = loadActions(current, "drop-commands", "drop_commands");
+        Object tag = null;
+        for (ITagProvider provider : tagProviders) {
+            if ((tag = provider.provide(current)) != null) {
+                break;
+            }
+        }
         this.tag = tag;
     }
 
+    /**
+     * 生成一个新的物品
+     * @param player 玩家，用于替换 PAPI 变量。使用 <code>null</code> 则不替换 PAPI 变量
+     * @see LoadedIcon#generateIcon(ItemStack, Player, IModifier, IModifier)
+     */
     public ItemStack generateIcon(Player player) {
         return generateIcon(player, null, null);
     }
 
+    /**
+     * 生成一个新的物品
+     * @param player 玩家，用于替换 PAPI 变量。使用 <code>null</code> 则不替换 PAPI 变量
+     * @param displayNameModifier 物品名称修饰器
+     * @param loreModifier 物品Lore修饰器
+     * @see LoadedIcon#generateIcon(ItemStack, Player, IModifier, IModifier)
+     */
     public ItemStack generateIcon(Player player, @Nullable IModifier<String> displayNameModifier, @Nullable IModifier<List<String>> loreModifier) {
         if (material.equals("AIR") || amount == 0) return new ItemStack(Material.AIR);
         Pair<Material, Integer> pair = ItemStackUtil.parseMaterial(this.material);
@@ -65,13 +143,50 @@ public class LoadedIcon {
         return generateIcon(item, player, displayNameModifier, loreModifier);
     }
 
-    public ItemStack generateIcon(ItemStack item, Player player) {
+    /**
+     * 基于已有物品，覆盖图标配置到该物品上。这个方法会忽略 <code>material</code> 选项。
+     * @param item 原物品
+     * @param player 玩家，用于替换 PAPI 变量。使用 <code>null</code> 则不替换 PAPI 变量
+     * @return <code>item</code> 的引用
+     * @see LoadedIcon#generateIcon(ItemStack, Player, IModifier, IModifier)
+     */
+    public ItemStack generateIcon(@Nullable ItemStack item, @Nullable Player player) {
         return generateIcon(item, player, null, null);
     }
 
-    public ItemStack generateIcon(ItemStack item, Player player, @Nullable IModifier<String> displayNameModifier, @Nullable IModifier<List<String>> loreModifier) {
+    /**
+     * 基于已有物品，覆盖图标配置到该物品上。这个方法会忽略 <code>material</code> 选项。
+     * @param item 原物品
+     * @param player 玩家，用于替换 PAPI 变量。使用 <code>null</code> 则不替换 PAPI 变量
+     * @param displayNameModifier 物品名称修饰器
+     * @param loreModifier 物品Lore修饰器
+     * @return 如果 <code>item</code> 不是 <code>null</code>，返回原物品的引用
+     */
+    public @NotNull ItemStack generateIcon(@Nullable ItemStack item, @Nullable Player player, @Nullable IModifier<String> displayNameModifier, @Nullable IModifier<List<String>> loreModifier) {
         if (item == null || amount == 0) return new ItemStack(Material.AIR);
         item.setAmount(amount);
+        applyItemMeta(item, player, displayNameModifier, loreModifier);
+        return item;
+    }
+
+    /**
+     * 应用该图标配置中的 物品名、物品Lore、发光、自定义标记… 等元数据到指定物品
+     * @param item 原物品
+     * @param player 玩家，用于替换 PAPI 变量。使用 <code>null</code> 则不替换 PAPI 变量
+     * @see LoadedIcon#applyItemMeta(ItemStack, Player, IModifier, IModifier)
+     */
+    public void applyItemMeta(@NotNull ItemStack item, @Nullable Player player) {
+        applyItemMeta(item, player, null, null);
+    }
+
+    /**
+     * 应用该图标配置中的 物品名、物品Lore、发光、自定义标记… 等元数据到指定物品
+     * @param item 原物品
+     * @param player 玩家，用于替换 PAPI 变量。使用 <code>null</code> 则不替换 PAPI 变量
+     * @param displayNameModifier 物品名称修饰器
+     * @param loreModifier 物品Lore修饰器
+     */
+    public void applyItemMeta(@NotNull ItemStack item, @Nullable Player player, @Nullable IModifier<String> displayNameModifier, @Nullable IModifier<List<String>> loreModifier) {
         if (!display.isEmpty()) {
             String displayName = PAPI.setPlaceholders(player, fit(displayNameModifier, display));
             if (adventure) AdventureItemStack.setItemDisplayName(item, displayName);
@@ -98,10 +213,24 @@ public class LoadedIcon {
                 }
             });
         }
-        return item;
     }
 
-    public void click(Player player, ClickType type) {
+    /**
+     * 处理图标点击操作
+     * @param player 点击该图标的玩家
+     * @param type 具体点击操作
+     */
+    public void click(@NotNull Player player, @NotNull ClickType type) {
+        click(player, type, null);
+    }
+
+    /**
+     * 处理图标点击操作
+     * @param player 点击该图标的玩家
+     * @param type 具体点击操作
+     * @param replacements 替换变量列表
+     */
+    public void click(@NotNull Player player, @NotNull ClickType type, @Nullable List<Pair<String, Object>> replacements) {
         List<IAction> actions;
         switch (type) {
             case LEFT:
@@ -122,58 +251,37 @@ public class LoadedIcon {
             default:
                 return;
         }
-        List<Pair<String, Object>> args = Lists.newArrayList(
-                Pair.of("__internal__loaded_icon", this)
-        );
+        ListPair<String, Object> args = new ListPair<>();
+        args.add("__internal__loaded_icon", this);
+        if (replacements != null) args.addAll(replacements);
         for (IAction action : actions) {
             action.run(player, args);
         }
     }
 
-    public static LoadedIcon load(@NotNull ConfigurationSection section) {
+    /**
+     * 加载图标配置
+     * @param section 配置根部分
+     */
+    public static @NotNull LoadedIcon load(@NotNull ConfigurationSection section) {
         return load(section, null);
     }
 
-    public static LoadedIcon load(@NotNull ConfigurationSection section, @Nullable String id) {
+    /**
+     * 加载图标配置
+     * @param section 配置
+     * @param id 配置根部分的位置键，若为 <code>null</code>，则配置根部分为 <code>section</code>
+     */
+    public static @NotNull LoadedIcon load(@NotNull ConfigurationSection section, @Nullable String id) {
         ConfigurationSection current = id == null ? section : section.getConfigurationSection(id);
-        assert current != null;
+        if (current == null) throw new IllegalArgumentException("Can't find root section of LoadedIcon");
 
-        String material, materialStr = current.getString("material");
-        if (materialStr != null) {
-            if (!materialStr.contains(":") && current.contains("data")) { // 兼容旧的选项
-                material = materialStr + ":" + current.getInt("data");
-            } else material = materialStr;
-        } else material = "PAPER";
-
-        int amount = current.getInt("amount", 1);
-        String display = current.getString("display", "");
-        List<String> lore = current.getStringList("lore");
-        boolean glow = current.getBoolean("glow");
-        Integer customModelData = current.contains("custom-model-data") ? current.getInt("custom-model-data") : null;
-        Map<String, String> nbtStrings = new HashMap<>();
-        ConfigurationSection section1 = current.getConfigurationSection("nbt-strings");
-        if (section1 != null) for (String key : section1.getKeys(false)) {
-            nbtStrings.put(key, section1.getString(key, ""));
-        }
-        Map<String, String> nbtInts = new HashMap<>();
-        section1 = current.getConfigurationSection("nbt-ints");
-        if (section1 != null) for (String key : section1.getKeys(false)) {
-            nbtInts.put(key, section1.getString(key, ""));
-        }
-        List<IAction> leftClickCommands = loadActions(current, "left-click-commands", "left_click_commands");
-        List<IAction> rightClickCommands = loadActions(current, "right-click-commands", "right_click_commands");
-        List<IAction> shiftLeftClickCommands = loadActions(current, "shift-left-click-commands", "shift_left_click_commands");
-        List<IAction> shiftRightClickCommands = loadActions(current, "shift-left-click-commands", "shift_left_click_commands");
-        List<IAction> dropCommands = loadActions(current, "drop-commands", "drop_commands");
-        Object tag = null;
-        for (ITagProvider provider : tagProviders) {
-            if ((tag = provider.provide(current)) != null) {
-                break;
-            }
-        }
-        return new LoadedIcon(BukkitPlugin.getInstance().options.adventure(), material, amount, display, lore, glow, customModelData, nbtStrings, nbtInts, leftClickCommands, rightClickCommands, shiftLeftClickCommands, shiftRightClickCommands, dropCommands, tag);
+        return new LoadedIcon(current);
     }
 
+    /**
+     * 注册 ITagProvider 标签提供器
+     */
     public static void registerTagProvider(ITagProvider provider) {
         tagProviders.add(provider);
         tagProviders.sort(Comparator.comparingInt(ITagProvider::priority));
