f89bb6bb336d896c10bc1515dca1b9153bf31fff
MrXiaoM <coolxiaom95@gmail.com>
2026-01-01 02:08:51 +0800

    add context generic parameter

--- modules/misc/src/main/java/top/mrxiaom/pluginbase/utils/ConditionalReplacer.java
+++ modules/misc/src/main/java/top/mrxiaom/pluginbase/utils/ConditionalReplacer.java
@@ -2,19 +2,19 @@ package top.mrxiaom.pluginbase.utils;
 
 import org.jetbrains.annotations.Nullable;
 
-public abstract class ConditionalReplacer {
+public abstract class ConditionalReplacer<Context> {
 
     @Nullable
-    public abstract String doReplace(String front, String input);
+    public abstract String doReplace(String front, String input, Context context);
 
-    public String replaceOrNull(String input) {
+    public String replaceOrNull(String input, Context context) {
         if (input.startsWith("$")) {
             String substring = input.substring(1);
             int index = substring.indexOf('$');
             if (index < 0) return null;
             String front = substring.substring(0, index);
             String newInput = substring.substring(index + 1);
-            return doReplace(front, newInput);
+            return doReplace(front, newInput, context);
         }
         return null;
     }
