fb15f94f98394700d5b6dcdc5b81ddd7e0461f52
MrXiaoM <coolxiaom95@gmail.com>
2026-01-01 20:42:52 +0800

    init sql module with nanoxml-lite

---
+++ modules/sql/README.md
@@ -0,0 +1,7 @@
+## sql
+
+PluginBase 的 SQL 生成模块。
+
+## 鸣谢
+
++ [NanoXML-lite](https://sourceforge.net/projects/nanoxml/files/NanoXML/2.2.1/) - zlib/libpng License
---
+++ modules/sql/build.gradle.kts
@@ -0,0 +1,9 @@
+
+dependencies {
+    applyLibraries("spigot-api", "compileOnly")
+}
+
+setupPublishing(
+    publishDesc = "MrXiaoM's Bukkit plugin basic core module generate sql sentence",
+    sourceCodeUrl = "https://github.com/MrXiaoM/PluginBase/tree/main/modules/sql",
+)
