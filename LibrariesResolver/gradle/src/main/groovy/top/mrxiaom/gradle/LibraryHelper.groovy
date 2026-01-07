0432b551852fdebdcbd3a105e9dc38db5e897440
MrXiaoM <coolxiaom95@gmail.com>
2026-01-07 14:41:00 +0800

    Update LibraryHelper.groovy

--- LibrariesResolver/gradle/src/main/groovy/top/mrxiaom/gradle/LibraryHelper.groovy
+++ LibrariesResolver/gradle/src/main/groovy/top/mrxiaom/gradle/LibraryHelper.groovy
@@ -10,13 +10,26 @@ import org.jetbrains.annotations.NotNull
 import org.jetbrains.annotations.Nullable
 
 import java.util.function.Consumer
+import java.util.function.Function
 
 class LibraryHelper {
     private final List<String> resolvedLibraries = new ArrayList<>()
     private Project project
+    /**
+     * 收集依赖用的配置
+     */
     private Configuration configuration
+    /**
+     * 添加依赖的目标配置，例如 compileOnly、implementation
+     */
     private String targetConfiguration
+    /**
+     * 当前版本的 LibrariesResolver 依赖简要声明字符串
+     */
     public final LibrariesResolver resolver = new LibrariesResolver()
+    /**
+     * 当前版本的 PluginBase 各模块依赖简要声明字符串
+     */
     public final PluginBase modules = new PluginBase()
     LibraryHelper(@NotNull Project project) {
         this(project, project.getConfigurations().create("libraryGroup", it -> {
@@ -86,9 +99,40 @@ class LibraryHelper {
         return resolvedLibraries
     }
 
+    static Function<ResolvedDependency, String> defaultCollector() {
+        return (dep) -> {
+            ResolvedArtifact artifact = Iterables.getFirst(dep.getModuleArtifacts(), null)
+            if (artifact == null) return null
+            String classifier = artifact.getClassifier()
+            String extension = artifact.getExtension() ?: "jar"
+            // 构建依赖文件路径
+            StringBuilder sb = new StringBuilder()
+            sb.append(dep.getModuleGroup().replace('.', '/'))
+            sb.append('/')
+            sb.append(dep.getModuleName())
+            sb.append('/')
+            sb.append(dep.getModuleVersion())
+            sb.append('/')
+            sb.append(dep.getModuleName())
+            sb.append('-')
+            sb.append(dep.getModuleVersion())
+            if (classifier != null) {
+                sb.append('-')
+                sb.append(classifier)
+            }
+            sb.append('.')
+            sb.append(extension)
+            return sb.toString()
+        }
+    }
+
     String join() {
+        return join(defaultCollector())
+    }
+
+    String join(Function<ResolvedDependency, String> collector) {
         if (resolvedLibraries.isEmpty()) {
-            doResolveLibraries()
+            doResolveLibraries(collector)
         }
         def joiner = new StringJoiner(",\n")
         for (final def s in resolvedLibraries) {
@@ -97,36 +141,33 @@ class LibraryHelper {
         return "new String[] {\n$joiner\n}"
     }
 
-    private void collect(ResolvedDependency dep) {
-        ResolvedArtifact artifact = Iterables.getFirst(dep.getModuleArtifacts(), null)
-        if (artifact == null) return
-        String classifier = artifact.getClassifier()
-        String extension = artifact.getExtension() ?: "jar"
-        // 构建依赖文件路径
-        StringBuilder sb = new StringBuilder()
-        sb.append(dep.getModuleGroup().replace('.', '/'))
-        sb.append('/')
-        sb.append(dep.getModuleName())
-        sb.append('/')
-        sb.append(dep.getModuleVersion())
-        sb.append('/')
-        sb.append(dep.getModuleName())
-        sb.append('-')
-        sb.append(dep.getModuleVersion())
-        if (classifier != null) {
-            sb.append('-')
-            sb.append(classifier)
-        }
-        sb.append('.')
-        sb.append(extension)
-        String s = sb.toString()
-        if (!resolvedLibraries.contains(s)) {
-            resolvedLibraries.add(s)
-            dep.getChildren().forEach(this::collect)
+    private <T> void collect(List<T> resolvedList, ResolvedDependency dep, Function<ResolvedDependency, T> collector) {
+        T s = collector.apply(dep)
+        if (s == null) return
+        if (!resolvedList.contains(s)) {
+            resolvedList.add(s)
+            for (final def child in dep.getChildren()) {
+                collect(resolvedList, child, collector)
+            }
         }
     }
+
+    <T> List<T> collectLibraries(Function<ResolvedDependency, T> collector) {
+        List<T> list = new ArrayList<>()
+        for (final def dependency in configuration.getResolvedConfiguration().getFirstLevelModuleDependencies()) {
+            collect(list, dependency, collector)
+        }
+        return list
+    }
+
     List<String> doResolveLibraries() {
-        configuration.getResolvedConfiguration().getFirstLevelModuleDependencies().forEach(this::collect)
+        return doResolveLibraries(defaultCollector())
+    }
+
+    List<String> doResolveLibraries(Function<ResolvedDependency, String> collector) {
+        for (final def dependency in configuration.getResolvedConfiguration().getFirstLevelModuleDependencies()) {
+            collect(resolvedLibraries, dependency, collector)
+        }
         return resolvedLibraries
     }
 }
