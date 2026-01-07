158d91d4d4728f20a2b5fdc8e1044027f3fd88ea
MrXiaoM <coolxiaom95@gmail.com>
2026-01-07 15:23:08 +0800

    update 1.7.3 without sql module yet

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,7 @@ fun includeProjects(name: String) {
     val files = File(name).listFiles() ?: return
     include(":$name")
     for (folder in files) {
+        if (folder.name == "sql") continue
         if (folder.isDirectory && File(folder, "build.gradle.kts").exists()) {
             include(":$name:${folder.name}")
         }
