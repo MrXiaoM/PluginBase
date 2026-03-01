e1ef64725ffb2812c830101a55633297c3cec3f8
MrXiaoM <coolxiaom95@gmail.com>
2026-03-01 10:11:20 +0800

    impl `Registry#match` by ourselves

--- modules/library/src/main/java/top/mrxiaom/pluginbase/utils/RegistryUtils.java
+++ modules/library/src/main/java/top/mrxiaom/pluginbase/utils/RegistryUtils.java
@@ -1,22 +1,32 @@
 package top.mrxiaom.pluginbase.utils;
 
 import org.bukkit.Keyed;
+import org.bukkit.NamespacedKey;
 import org.bukkit.Registry;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class RegistryUtils {
     @SuppressWarnings("unchecked")
     public static <T> T fromType(Class<T> type, String s) {
         Registry<?> registry = RegistryConverter.fromType(type);
         if (registry != null) {
-            Keyed matched = registry.match(s);
+            Keyed matched = match(registry, s);
             if (/*matched != null && */type.isInstance(matched)) {
                 return (T) matched;
             }
-            Keyed newerMatched = registry.match(s.replace('_', '.'));
+            Keyed newerMatched = match(registry, s.replace('_', '.'));
             if (/*newerMatched != null && */type.isInstance(matched)) {
                 return (T) newerMatched;
             }
         }
         return null;
+    }
+
+    @Nullable
+    public static <T extends Keyed> T match(@NotNull Registry<T> registry, @NotNull String input) {
+        String filtered = input.toLowerCase().replaceAll("\\s+", "_");
+        NamespacedKey namespacedKey = NamespacedKey.fromString(filtered);
+        return (namespacedKey != null) ? registry.get(namespacedKey) : null;
     }
 }
