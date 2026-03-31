7fc5c56d1552026b4be3a89b8c641d2e72986acc
MrXiaoM <coolxiaom95@gmail.com>
2026-03-31 13:57:22 +0800

    add more collection helper

--- modules/library/src/main/java/top/mrxiaom/pluginbase/utils/CollectionUtils.java
+++ modules/library/src/main/java/top/mrxiaom/pluginbase/utils/CollectionUtils.java
@@ -2,25 +2,123 @@ package top.mrxiaom.pluginbase.utils;
 
 import com.google.common.collect.Lists;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Range;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class CollectionUtils {
+
+    /**
+     * 转换集合中的每一个元素，创建新的列表
+     * @param collection 待转换的集合
+     * @param transformer 转换器
+     * @return 新的列表
+     */
+    public static <T, R> List<R> map(Collection<T> collection, Function<T, R> transformer) {
+        List<R> list = new ArrayList<>();
+        for (T t : collection) {
+            list.add(transformer.apply(t));
+        }
+        return list;
+    }
+
+    /**
+     * 转换集合中的每一个元素，创建新的列表，略过转换为 <code>null</code> 的元素
+     * @param collection 待转换的集合
+     * @param transformer 转换器
+     * @return 新的列表
+     */
+    public static <T, R> List<R> mapNonNull(Collection<T> collection, Function<T, R> transformer) {
+        List<R> list = new ArrayList<>();
+        for (T t : collection) {
+            R result = transformer.apply(t);
+            if (result != null) {
+                list.add(result);
+            }
+        }
+        return list;
+    }
+
+    /**
+     * 过滤集合中的元素，将满足条件的元素添加到新的列表
+     * @param collection 待转换的集合
+     * @param predicate 过滤条件
+     * @return 新的列表
+     */
+    public static <T> List<T> filter(Collection<T> collection, Predicate<T> predicate) {
+        List<T> list = new ArrayList<>();
+        for (T t : collection) {
+            if (predicate.test(t)) {
+                list.add(t);
+            }
+        }
+        return list;
+    }
+
+    /**
+     * 过滤集合中的元素，将指定类型的元素添加到新的列表
+     * @param collection 待转换的集合
+     * @param type 类型
+     * @return 新的列表
+     */
+    public static <T, R> List<R> filterIsInstance(Collection<T> collection, Class<R> type) {
+        List<R> list = new ArrayList<>();
+        for (T t : collection) {
+            if (type.isInstance(t)) {
+                //noinspection unchecked
+                list.add((R) t);
+            }
+        }
+        return list;
+    }
+
+    /**
+     * 获取集合中符合条件的元素数量
+     * @param collection 待判定集合
+     * @param predicate 判定条件
+     * @return 元素数量
+     */
+    @Range(from = 0, to = Integer.MAX_VALUE)
+    public static <T> int count(Collection<T> collection, Predicate<T> predicate) {
+        int count = 0;
+        for (T t : collection) {
+            if (predicate.test(t)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    /**
+     * 分割字符串
+     * @param string 待分割的字符串
+     * @param splitter 分割符
+     * @return 分割后的列表，至少有 1 个元素
+     */
     public static List<String> split(String string, String splitter) {
         return split(string, splitter, 0);
     }
 
+    /**
+     * 分割字符串
+     * @param string 待分割的字符串
+     * @param splitter 分割符
+     * @param limit 限制最多分割为多少份，<code>0</code> 代表不限制
+     * @return 分割后的列表，至少有 1 个元素
+     */
     public static List<String> split(String string, String splitter, int limit) {
         int oldIndex = 0;
         int length = splitter.length();
-        if (length == 0) throw new IllegalArgumentException("spliter can't be empty!");
+        if (length == 0) throw new IllegalArgumentException("splitter can't be empty!");
         int count = limit - 1;
         boolean unlimited = limit <= 0;
         List<String> list = new ArrayList<>();
@@ -41,10 +139,23 @@ public class CollectionUtils {
         return list;
     }
 
+    /**
+     * 分割字符串
+     * @param string 待分割的字符串
+     * @param splitter 分割符
+     * @return 分割后的列表，至少有 1 个元素
+     */
     public static List<String> split(String string, char splitter) {
         return split(string, splitter, 0);
     }
 
+    /**
+     * 分割字符串
+     * @param string 待分割的字符串
+     * @param splitter 分割符
+     * @param limit 限制最多分割为多少份，<code>0</code> 代表不限制
+     * @return 分割后的列表，至少有 1 个元素
+     */
     public static List<String> split(String string, char splitter, int limit) {
         int oldIndex = 0;
         int count = limit - 1;
@@ -110,7 +221,7 @@ public class CollectionUtils {
      * @param list 列表
      * @param size 每份多少个元素
      */
-    public static <T> List<List<T>> chunk(List<T> list, int size) {
+    public static <T> List<List<T>> chunk(Collection<T> list, int size) {
         List<List<T>> result = new ArrayList<>();
         List<T> temp = new ArrayList<>();
         for (T item : list) {
