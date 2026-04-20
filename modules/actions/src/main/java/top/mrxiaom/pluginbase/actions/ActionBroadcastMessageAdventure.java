79eece6e33b5503727990a4f7b8ccab666f6c7c0
MrXiaoM <coolxiaom95@gmail.com>
2026-04-20 13:57:36 +0800

    add adventure hover and click format for message actions

--- modules/actions/src/main/java/top/mrxiaom/pluginbase/actions/ActionBroadcastMessageAdventure.java
+++ modules/actions/src/main/java/top/mrxiaom/pluginbase/actions/ActionBroadcastMessageAdventure.java
@@ -1,8 +1,11 @@
 package top.mrxiaom.pluginbase.actions;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.event.ClickEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import top.mrxiaom.pluginbase.api.IAction;
 import top.mrxiaom.pluginbase.api.IActionProvider;
@@ -19,41 +22,52 @@ public class ActionBroadcastMessageAdventure implements IAction {
             if (!section.contains("type") && section.contains("broadcast")) {
                 String content = section.getString("broadcast");
                 if (content != null) {
-                    return new ActionBroadcastMessageAdventure(content);
+                    return new ActionBroadcastMessageAdventure(content, null, null, null);
                 }
             } else if ("broadcast".equals(section.getString("type"))) {
                 String content = section.getString("content");
                 if (content != null) {
-                    return new ActionBroadcastMessageAdventure(content);
+                    List<String> hoverLines = section.contains("hover") ? section.getStringList("hover") : null;
+                    String clickAction = section.getString("click.action", "RUN_COMMAND");
+                    String clickValue = section.getString("click.value", null);
+                    return new ActionBroadcastMessageAdventure(content, hoverLines, clickAction, clickValue);
                 }
             }
         } else {
             String s = String.valueOf(input);
             if (s.startsWith("[broadcast]")) {
-                return new ActionBroadcastMessageAdventure(s.substring(11));
+                return new ActionBroadcastMessageAdventure(s.substring(11), null, null, null);
             }
             if (s.startsWith("broadcast:")) {
-                return new ActionBroadcastMessageAdventure(s.substring(10));
+                return new ActionBroadcastMessageAdventure(s.substring(10), null, null, null);
             }
         }
         return null;
     };
-    public final String message;
-    public ActionBroadcastMessageAdventure(String message) {
+    public final @NotNull String message;
+    public final @Nullable List<String> hoverLines;
+    public final @Nullable String clickAction;
+    public final @Nullable String clickValue;
+    public ActionBroadcastMessageAdventure(@NotNull String message, @Nullable List<String> hoverLines, @Nullable String clickAction, @Nullable String clickValue) {
         this.message = message;
+        this.hoverLines = hoverLines;
+        this.clickAction = clickAction == null ? null : clickAction.replace("-", "_").toUpperCase();
+        this.clickValue = clickValue;
     }
 
     @Override
     public void run(@Nullable Player player, @Nullable List<Pair<String, Object>> replacements) {
-        String str;
+        Component component;
         if (player != null) {
-            str = PAPI.setPlaceholders(player, Pair.replace(message, replacements));
+            String str = PAPI.setPlaceholders(player, Pair.replace(message, replacements));
+            component = ActionMessageAdventure.parseComponent(str, hoverLines, clickAction, clickValue);
         } else {
-            str = Pair.replace(message, replacements);
+            String str = Pair.replace(message, replacements);
+            component = ActionMessageAdventure.parseComponent(str, hoverLines, clickAction, clickValue);
         }
-        AdventureUtil.sendMessage(Bukkit.getConsoleSender(), str);
+        AdventureUtil.sendMessage(Bukkit.getConsoleSender(), component);
         for (Player p : Bukkit.getOnlinePlayers()) {
-            AdventureUtil.sendMessage(p, str);
+            AdventureUtil.sendMessage(p, component);
         }
     }
 }
--- modules/actions/src/main/java/top/mrxiaom/pluginbase/actions/ActionMessageAdventure.java
+++ modules/actions/src/main/java/top/mrxiaom/pluginbase/actions/ActionMessageAdventure.java
@@ -1,8 +1,11 @@
 package top.mrxiaom.pluginbase.actions;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.event.ClickEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import top.mrxiaom.pluginbase.api.IAction;
 import top.mrxiaom.pluginbase.api.IActionProvider;
@@ -19,37 +22,76 @@ public class ActionMessageAdventure implements IAction {
             if (!section.contains("type") && section.contains("message")) {
                 String content = section.getString("message");
                 if (content != null) {
-                    return new ActionMessageAdventure(content);
+                    return new ActionMessageAdventure(content, null, null, null);
                 }
             } else if ("message".equals(section.getString("type"))) {
                 String content = section.getString("content");
                 if (content != null) {
-                    return new ActionMessageAdventure(content);
+                    List<String> hoverLines = section.contains("hover") ? section.getStringList("hover") : null;
+                    String clickAction = section.getString("click.action", "RUN_COMMAND");
+                    String clickValue = section.getString("click.value", null);
+                    return new ActionMessageAdventure(content, hoverLines, clickAction, clickValue);
                 }
             }
         } else {
             String s = String.valueOf(input);
             if (s.startsWith("[message]")) {
-                return new ActionMessageAdventure(s.substring(9));
+                return new ActionMessageAdventure(s.substring(9), null, null, null);
             }
             if (s.startsWith("message:")) {
-                return new ActionMessageAdventure(s.substring(8));
+                return new ActionMessageAdventure(s.substring(8), null, null, null);
             }
         }
         return null;
     };
-    public final String message;
-    public ActionMessageAdventure(String message) {
+    public final @NotNull String message;
+    public final @Nullable List<String> hoverLines;
+    public final @Nullable String clickAction;
+    public final @Nullable String clickValue;
+    public ActionMessageAdventure(@NotNull String message, @Nullable List<String> hoverLines, @Nullable String clickAction, @Nullable String clickValue) {
         this.message = message;
+        this.hoverLines = hoverLines;
+        this.clickAction = clickAction;
+        this.clickValue = clickValue;
     }
 
     @Override
     public void run(@Nullable Player player, @Nullable List<Pair<String, Object>> replacements) {
         String s = Pair.replace(message, replacements);
+        Component component;
         if (player != null) {
-            AdventureUtil.sendMessage(player, PAPI.setPlaceholders(player, s));
+            component = parseComponent(PAPI.setPlaceholders(player, Pair.replace(message, replacements)), hoverLines, clickAction, clickValue);
+            AdventureUtil.sendMessage(player, component);
         } else {
-            AdventureUtil.sendMessage(Bukkit.getConsoleSender(), s);
+            component = parseComponent(Pair.replace(message, replacements), hoverLines, clickAction, clickValue);
+            AdventureUtil.sendMessage(Bukkit.getConsoleSender(), component);
         }
+    }
+
+    protected static Component parseComponent(String str, List<String> hoverLines, String clickAction, String clickValue) {
+        Component component = AdventureUtil.miniMessage(str);
+        if (hoverLines != null) {
+            component = component.hoverEvent(AdventureUtil.miniMessageLines(hoverLines));
+        }
+        if (clickAction != null && clickValue != null) {
+            switch (clickAction) {
+                case "RUN_COMMAND":
+                    component = component.clickEvent(ClickEvent.runCommand(clickValue));
+                    break;
+                case "SUGGEST_COMMAND":
+                    component = component.clickEvent(ClickEvent.suggestCommand(clickValue));
+                    break;
+                case "COPY_TO_CLIPBOARD":
+                    component = component.clickEvent(ClickEvent.copyToClipboard(clickValue));
+                    break;
+                case "OPEN_URL":
+                    component = component.clickEvent(ClickEvent.openUrl(clickValue));
+                    break;
+                case "OPEN_FILE":
+                    component = component.clickEvent(ClickEvent.openFile(clickValue));
+                    break;
+            }
+        }
+        return component;
     }
 }
