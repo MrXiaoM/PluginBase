4b899c604f9854a0ddfc63bf51cbccbc03c6cc0f
MrXiaoM <coolxiaom95@gmail.com>
2026-05-10 21:51:38 +0800

    add handy builtin IModifier for ListPair replacements

--- modules/gui/src/main/java/top/mrxiaom/pluginbase/func/gui/IModifier.java
+++ modules/gui/src/main/java/top/mrxiaom/pluginbase/func/gui/IModifier.java
@@ -2,6 +2,9 @@ package top.mrxiaom.pluginbase.func.gui;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import top.mrxiaom.pluginbase.utils.Pair;
+
+import java.util.List;
 
 /**
  * 数值修饰器，常用于让开发者可修改物品的名称、lore 等显示文字
@@ -14,5 +17,13 @@ public interface IModifier<T> {
     @NotNull
     static <T> T fit(@Nullable IModifier<T> modifier, @NotNull T value) {
         return modifier == null ? value : modifier.run(value);
+    }
+
+    static IModifier<String> replaceString(List<Pair<String, Object>> r) {
+        return r == null ? null : (input -> Pair.replace(input, r));
+    }
+
+    static IModifier<List<String>> replaceList(List<Pair<String, Object>> r) {
+        return r == null ? null : (input -> Pair.replace(input, r));
     }
 }
